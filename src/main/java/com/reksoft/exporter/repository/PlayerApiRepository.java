3bb00ac3ff02823bec4593fec84b01ed15e151b5
Ilia Lysov <illigiorno@gmail.com>
2025-07-04 21:27:57 +0400

    Added logs for PlayerApiRepository

--- src/main/java/com/reksoft/exporter/repository/PlayerApiRepository.java
+++ src/main/java/com/reksoft/exporter/repository/PlayerApiRepository.java
@@ -3,6 +3,9 @@ package com.reksoft.exporter.repository;
 import com.reksoft.exporter.properties.ApiProperties;
 import com.reksoft.exporter.repository.dto.PlayerViewDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +17,11 @@ import java.util.List;
 
 import static org.springframework.http.HttpMethod.GET;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class PlayerApiRepository implements PlayerRepository {
+    private static final Logger logger = LoggerFactory.getLogger(PlayerApiRepository.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -24,29 +29,44 @@ public class PlayerApiRepository implements PlayerRepository {
     private ApiProperties apiProperties;
 
     public List<PlayerViewDto> getPlayers() {
-        ResponseEntity<List<PlayerViewDto>> response = restTemplate.exchange(
-                apiProperties.getBaseUrl() + "/api/Players",
-                GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
-        return response.getBody();
+        logger.info("Attempting to get players list from API");
+        String url = apiProperties.getBaseUrl() + "/api/Players";
+
+        try {
+            ResponseEntity<List<PlayerViewDto>> response = restTemplate.exchange(
+                    url,
+                    GET,
+                    null,
+                    new ParameterizedTypeReference<>() {}
+            );
+
+            logger.info("Successfully retrieved {} players", response.getBody() != null ? response.getBody().size() : 0);
+            return response.getBody();
+        } catch (Exception e) {
+            logger.error("Error while getting players from API. URL: {}", url, e);
+            throw e;
+        }
     }
 
     public List<PlayerViewDto> getFilteredPlayers(String filter) {
+        logger.info("Attempting to get filtered players with filter: '{}'", filter);
         String uri = UriComponentsBuilder.fromHttpUrl(apiProperties.getBaseUrl() + "/api/Players/filter")
                 .queryParam("filter", filter)
                 .toUriString();
 
-        ResponseEntity<List<PlayerViewDto>> response = restTemplate.exchange(
-                uri,
-                GET,
-                null,
-                new ParameterizedTypeReference<>() {
-                }
-        );
-        return response.getBody();
-    }
+        try {
+            ResponseEntity<List<PlayerViewDto>> response = restTemplate.exchange(
+                    uri,
+                    GET,
+                    null,
+                    new ParameterizedTypeReference<>() {}
+            );
 
+            logger.info("Successfully retrieved {} filtered players", response.getBody() != null ? response.getBody().size() : 0);
+            return response.getBody();
+        } catch (Exception e) {
+            logger.error("Error while getting filtered players from API. URL: {}, filter: {}", uri, filter, e);
+            throw e;
+        }
+    }
 }
